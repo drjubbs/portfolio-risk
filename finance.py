e0bcf294074062b0a012e4d81d5edec91f6f4f7e
drjubbs <spam@pm.com>
2021-11-19 17:28:54 -0500

    Added finance.create_flows. IRR is not a useful measure of return, need to rework logic on calculating return on investment.

--- finance.py
+++ finance.py
@@ -3,8 +3,10 @@
 """Financial functions used to calculate net present value and internal rates of
 return.
 """
+import datetime as dt
 from functools import partial
 from scipy.optimize import root
+import pandas as pd
 
 
 def calc_npv(df_in, discount):
@@ -12,16 +14,16 @@ def calc_npv(df_in, discount):
     representing data and cash flow. Negative cash flows are investments,
     positive cash flows are divestments.
 
-    `discount` is the discount rate used in percent
+    `discount` is the yearly discount rate used, in percent.
     """
 
     df_in = df_in.sort_values(by='datetime')
     df_in.reset_index(inplace=True)
     df_in['time_delta'] = df_in['datetime'] - df_in.datetime[0]
-    df_in['years_delta'] = [t.total_seconds() / (365 * 24 * 60 * 60) for t in
+    df_in['years_delta'] = [t.total_seconds() / (365.25 * 24 * 60 * 60) for t in
                             df_in['time_delta']]
 
-    # Average out leap year effects, found
+    # Average out leap year effects
     df_in['years_delta'] = [round(t, 2) for t in df_in['years_delta']]
 
     # Calculate net present value at oldest date
@@ -37,7 +39,9 @@ def calc_npv(df_in, discount):
 
 
 def calc_internal_ror(df_in):
-    """Calculate internal rate of return, similar to Excel IRR function"""
+    """Calculate internal rate of return, similar to Excel IRR function.
+    Note that this will return a ROR, compoundly yearly.
+    """
     df_in = df_in.sort_values(by='datetime')
     f_partial = partial(calc_npv, df_in)
     soln = root(f_partial, x0=6, method='lm')
@@ -64,3 +68,46 @@ def return_closest_time(df_in, date_in):
     time_deltas = abs(df_in.datetime-date_in)
     mask = time_deltas == min(time_deltas)
     return df_in[mask].iloc[0, :]
+
+
+def create_flows(df_dates, in_amount, dt_begin, in_offsets, out_offset):
+    """Create a Pandas DataFrame for use in cash flow calculations.
+
+    `df_dates`: DataFrame with a `datetime` column and `adj_close` column
+                which contains a "database" of index values. For example, this
+                might be a price level index for the S&P 500 index.
+    `in_amount`: Amount of inital investment. Will be split into
+                `len(in_spacing)` chunks.
+    `in_begin`: Datetime object of when to start the initial investment.
+    `in_offsets`: list of offsets, in days, over which the investment will
+                  be spread out.
+    `out_offset`: days corresponding to when divestment occurs.
+    """
+
+    # If invalid beginning time is given find the closest date
+    dt_begin = return_closest_time(df_dates, dt_begin).datetime.to_pydatetime()
+
+    # Create investment part of DataFrame
+    in_rows = []
+    for offset in in_offsets:
+        in_rows.append(return_closest_time(
+                            df_dates, dt_begin+dt.timedelta(days=offset)))
+    df_in_flows = pd.DataFrame(in_rows)
+
+    invest_amount = in_amount / len(in_offsets)
+    df_in_flows['shares'] = invest_amount/df_in_flows['adj_close']
+    df_in_flows['flows'] = -1.0*invest_amount
+    shares = sum(df_in_flows.shares)
+
+    # Divestment section
+    dt_end = return_closest_time(df_dates, dt_begin+dt.timedelta(
+        days=out_offset))
+    dt_end['flows'] = shares*dt_end['adj_close']
+    dt_end['shares'] = -shares
+    df_out_flows = pd.DataFrame([dt_end])
+
+    # Let's be paranoid and make sure columns line up
+    df_in_flows = df_in_flows[['datetime', 'adj_close', 'shares', 'flows']]
+    df_out_flows = df_out_flows[['datetime', 'adj_close', 'shares', 'flows']]
+
+    return df_in_flows.append(df_out_flows)
--- testing.py
+++ testing.py
@@ -23,7 +23,7 @@ class TestFinance(unittest.TestCase):
         })
 
         self.assertTrue(np.isclose(
-            finance.calc_npv(example1, discount=6), -25.6604))
+            finance.calc_npv(example1, discount=6), -25.6603773))
 
         # Example 2 NPV: Mix up date order
         dates = [dt.datetime(1983, 1, 1),
@@ -42,6 +42,22 @@ class TestFinance(unittest.TestCase):
 
         self.assertTrue(np.isclose(
             finance.calc_internal_ror(example2), 8.5683))
+
+        # Example 3 NPV: Three investments
+        dates = [dt.datetime(1979, 8, 1),
+                 dt.datetime(1980, 8, 1),
+                 dt.datetime(1981, 8, 1),
+                 dt.datetime(1984, 8, 1)
+                 ]
+        flows = [-333.33, -333.33, -333.33, 1312.88]
+        example3 = pd.DataFrame({
+            'datetime': dates,
+            'flows': flows,
+        })
+
+        self.assertAlmostEqual(finance.calc_internal_ror(example3),
+                               7.0019386,
+                               5)
 
     def test_finddates(self):
         """Tests for finding closest date in a DataFrame"""
@@ -74,6 +90,51 @@ class TestFinance(unittest.TestCase):
             row = finance.return_closest_time(df_test, dt.datetime(2021, 7, 24))
             self.assertEqual(row.datetime.to_pydatetime(),
                              dt.datetime(2021, 6, 25))
+
+    def test_create_flows(self):
+        """Test routines which create cash flow DataFrames for NPV
+        calculations."""
+
+        # Create scratch DataFrame covering 30 years with a 7% ROR
+        # This is a bit tricky we need to be careful with daily vs. yealy
+        # compounding.
+        dates = []
+        adj_close = []
+        x0 = 100        # Starting value for index
+        rate = 0.07     # Rate of return
+        for i in range(30):
+            for j in range(12):
+                dates.append(dt.datetime(1970+i, j+1, 1))
+                num_years = (dates[-1]-dates[0]).days/365.25
+                adj_close.append(x0*(1+rate)**num_years)
+
+        df_test = pd.DataFrame({'datetime': dates, 'adj_close': adj_close})
+
+        # Beginning of DataFrame frame, single investment, 10 years out
+        df_flows1 = finance.create_flows(
+                                    df_test,
+                                    1000,
+                                    dt.datetime(1970, 1, 1),
+                                    [0],
+                                    int(365.25*10))
+        self.assertEqual(2, len(df_flows1))
+        self.assertEqual(dt.datetime(1980, 1, 1),
+                         df_flows1.iloc[-1, :].datetime.to_pydatetime())
+
+        # IRR should be near 7...
+        delta_rate = (7.0 - finance.calc_internal_ror(df_flows1))
+        self.assertTrue(delta_rate < 0.01)
+
+        # Offset in dataframe, investment split up...
+        df_flows2 = finance.create_flows(
+                                    df_test,
+                                    1000,
+                                    dt.datetime(1979, 7, 23),
+                                    [0, 365, 2*365],
+                                    int(365.25*5))
+
+        delta_rate = (7.0 - finance.calc_internal_ror(df_flows2))
+        self.assertTrue(delta_rate < 0.01)
 
 
 if __name__ == '__main__':
